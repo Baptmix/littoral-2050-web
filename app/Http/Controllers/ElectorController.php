d2c62dd7bac542dca39b13e6da8063c92724c22a
Baptiste Jourquin <baptiste@webilo.fr>
2022-01-16 23:18:03 +0100

    Update for fix

--- app/Http/Controllers/ElectorController.php
+++ app/Http/Controllers/ElectorController.php
@@ -9,15 +9,11 @@ class ElectorController extends Controller
 {
     public function edit(int $id) {
         $client = new Client();
-        $response = $client->request('POST', env("APP_API_URL") . "/elector/find", [
+        $response = $client->request('GET', env("APP_API_URL") . "/elector/" . $id, [
             'headers' => [
                 'Authorization' => 'Bearer '. session()->get("token"),
                 'Accept' => 'application/json',
             ],
-
-            'form_params' => [
-                "id" => $id
-            ]
         ]);
 
         $citiesRequest = $client->request('GET', env("APP_API_URL") . "/electors/cities/all", [
