2e60789211a3dd0b7d7bac8e9ed0f6d600c6d5ed
Baptmix <baptiste.jourq1@gmail.com>
2022-01-31 15:19:40 +0100

    Ajout de la page engagement de confidentialité

---
+++ resources/views/engagement-de-confidentialite.blade.php
@@ -0,0 +1,147 @@
+@extends('layouts.app')
+
+@section('title', 'Engagement de confindentialité')
+
+@section('content')
+    <style>
+        .content h2 {
+            text-transform: uppercase;
+            background: linear-gradient(to right, #6ce7df 0%, #4967e5 100%);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+    </style>
+    <section class="content">
+        <div class="min-h-full flex items-center justify-center">
+            <div class="w-full space-y-8">
+                <div>
+                    <img class="mx-auto h-20 w-auto" src="{{ asset('svg/logo.svg') }}"
+                         alt="Workflow">
+                    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Engagement de confidentialité
+                    </h2>
+                </div>
+                <div>
+                    <p class="lead">
+                        <strong>ARTICLE 1 : PRÉAMBULE</strong><br>
+                        Cette politique de confidentialité s'applique au site littoral-app.fr.
+                        La présente politique de confidentialité a pour but d'exposer aux utilisateurs du site :
+                        La manière dont sont collectées et traitées leurs données à caractère personnel.
+                        <br>
+                        <br>
+                        Doivent être considérées comme données personnelles toutes les données étant susceptibles
+                        d'identifier un
+                        utilisateur. Il s'agit notamment du nom d'utilisateur, l'adresse
+                        mail, la localisation de l'utilisateur ou encore son adresse IP.
+                        Quels sont les droits des utilisateurs concernant ces données ?
+                        Qui est responsable du traitement des données à caractère personnel collectées et traitées ?
+                        <br><br><u>A qui ces données sont transmises ?</u><br>
+                        Eventuellement, la politique du site en matière de fichiers "cookies".
+                        Cette politique de confidentialité complète les mentions légales et les Conditions Générales
+                        d'Utilisation que les utilisateurs peuvent consulter à l'adresse ci-après :
+                        www.littoral-app.fr
+
+                        <br><br><strong>ARTICLE 2: PRINCIPES GÉNÉRAUX EN MATIÈRE DE COLLECTE ET DE TRAITEMENT DE
+                            DONNÉES</strong><br>
+                        Conformément aux dispositions de l'article 5 du Règlement européen 2016/679, la collecte et le
+                        traitement des données des utilisateurs du site respectent les principes suivants :
+                        Licéité, loyauté et transparence : les données ne peuvent être collectées et traitées
+                        qu'avec le consentement de l'utilisateur propriétaire des données. A chaque fois que des données
+                        à caractère personnel seront collectées, il sera indiqué à l'utilisateur que ses données sont
+                        collectées, et pour quelles raisons ses données sont collectées ;
+                        Finalités limitées : la collecte et le traitement des données sont exécutés pour répondre à
+                        un ou plusieurs objectifs déterminés dans les présentes conditions générales d'utilisation ;
+                        Minimisation de la collecte et du traitement des données : seules les données nécessaires à
+                        la bonne exécution des objectifs poursuivis par le site sont collectées ;
+                        Conservation des données réduites dans le temps : les données sont conservées pour une
+                        durée limitée, dont l'utilisateur est informé. Lorsque cette information ne peut pas être
+                        communiquée, l'utilisateur est informé des critères utilisés pour déterminer la durée de
+                        conservation ;
+                        Intégrité et confidentialité des données collectées et traitées : le responsable du
+                        traitement des données s'engage à garantir l'intégrité et la confidentialité des données
+                        collectées.
+                        <br><br>Afin d'être licites, et ce conformément aux exigences de l'article 6 du règlement
+                        européen 2016/679, la collecte et le traitement des données à caractère personnel ne pourront
+                        intervenir que s'ils respectent au moins l'une des conditions ci-après énumérées :<br><br>
+                        L'utilisateur a expressément consenti au traitement ;<br>
+                        Le traitement est nécessaire à la bonne exécution d'un contrat ;<br>
+                        Le traitement répond à une obligation légale ;<br>
+                        Le traitement s'explique par une nécessité liée à la sauvegarde des intérêts vitaux de la
+                        personne concernée ou d'une autre personne physique ;<br>
+                        Le traitement peut s'expliquer par une nécessité liée à l'exécution d'une mission d'intérêt
+                        public ou qui relève de l'exercice de l'autorité publique ;<br>
+                        Le traitement et la collecte des données à caractère personnel sont nécessaires aux fins des
+                        intérêts légitimes et privés poursuivis par le responsable du traitement ou par un tiers.
+
+                        <br><br><strong>ARTICLE 3 : DONNÉES À CARACTÈRE PERSONNEL COLLECTÉES ET TRAITÉES DANS LE
+                            CADRE DE LA NAVIGATION SUR LE SITE</strong><br>
+                        <u>A. DONNÉES COLLECTÉES ET TRAITÉES ET MODE DE COLLECTE</u><br>
+                        Les données à caractère personnel collectées sur le site www.littoral-app.fr sont les suivantes
+                        :
+                        Nom d'utilisateur, adresse e-mail.
+                        Ces données sont collectées lorsque l'utilisateur est créé par un membre de l'organisation :
+                        Inscription sur le site ; Mise à jour des informations d'identité
+                        Le responsable du traitement conservera dans ses systèmes informatiques du site et dans des
+                        conditions raisonnables de sécurité l'ensemble des données collectées pour une durée de :
+                        24 mois.
+                        -Consentement donné par l'utilisateur
+                        <u>B. TRANSMISSION DES DONNÉES A DES TIERS</u><br>
+                        Les données à caractère personnel collectées par le site ne sont transmises à aucun tiers, et ne
+                        sont traitées que par l'éditeur du site.
+                        <u>C. HÉBERGEMENT DES DONNÉES</u><br>
+                        Le site www.littoral-app.fr est hébergé : OVH.
+                        Les données collectées et traitées par le site sont exclusivement hébergées et traitées en
+                        France.
+
+                        <br><br><strong>ARTICLE 4 : RESPONSABLE DU TRAITEMENT DES DONNÉES ET DÉLÉGUÉ À LA
+                            PROTECTION DES DONNÉES</strong><br>
+                        <u>OBLIGATIONS DU RESPONSABLE DU TRAITEMENT DES DONNÉES</u><br>
+                        Le responsable du traitement s'engage à protéger les données à caractère personnel collectées, à
+                        ne pas les transmettre à des tiers sans que l'utilisateur n'en ait été informé et à respecter
+                        les finalités pour lesquelles ces données ont été collectées.
+                        Le site dispose d'un certificat SSL afin de garantir que les informations et le transfert des
+                        données transitant par le site sont sécurisés.
+                        Un certificat SSL ("Secure Socket Layer" Certificate) a pour but de sécuriser les données
+                        échangées entre l'utilisateur et le site.
+                        De plus, le responsable du traitement des données s'engage à notifier l'utilisateur en cas de
+                        rectification ou de suppression des données, à moins que cela n'entraîne pour lui des
+                        formalités, coûts et démarches disproportionnés.
+                        Dans le cas où l'intégrité, la confidentialité ou la sécurité des données à caractère personnel
+                        de l'utilisateur est compromise, le responsable du traitement s'engage à informer l'utilisateur
+                        par tout moyen.
+                        <br><br><strong>ARTICLE 5 : DROITS DE L'UTILISATEUR</strong><br>
+                        Conformément à la réglementation concernant le traitement des données à caractère personnel,
+                        l'utilisateur possède les droits ci-après énumérés.
+                        Afin que le responsable du traitement des données fasse droit à sa demande, l'utilisateur est
+                        tenu de lui communiquer : son adresse e-mail.
+                        Le responsable du traitement des données est tenu de répondre à l'utilisateur dans un délai de
+                        30 (trente) jours maximum.
+
+                        <br><br><strong>ARTICLE 6 : UTILISATION DES FICHIERS "COOKIES"</strong><br>
+                        Le site n'a pas recours aux cookies. Aucune donnée n'est sauvegardée dans le navigateur hormis le token.
+                        <br>
+                        ________
+                        <br><br><strong>ARTICLE 7 : CONDITIONS DE MODIFICATION DE LA POLITIQUE DE
+                            CONFIDENTIALITÉ</strong><br>
+                        La présente politique de confidentialité peut être consultée à tout moment à l'adresse ci-après
+                        indiquée:<br> www.littoral-app.fr
+                        <br><br>L'éditeur du site se réserve le droit de la modifier afin de garantir sa conformité avec
+                        le droit en vigueur.
+                        Par conséquent, l'utilisateur est invité à venir consulter régulièrement cette politique de
+                        confidentialité afin de se tenir informé des derniers changements qui lui seront apportés.
+                        <br><br>Il est porté à la connaissance de l'utilisateur que la dernière mise à jour de la
+                        présente politique de confidentialité est intervenue le : 31/01/2022.
+                        <br><br><strong>ARTICLE 8 : ACCEPTATION PAR L'UTILISATEUR DE LA POLITIQUE DE
+                            CONFIDENTIALITÉ</strong><br>
+                        En naviguant sur le site, l'utilisateur atteste avoir lu et compris la présente politique de
+                        confidentialité et en accepte les conditions, en ce qui concerne plus particulièrement la
+                        collecte et le traitement de ses données à caractère personnel, ainsi que l'utilisation de
+                        fichiers "cookies".
+                    </p>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+@stop
