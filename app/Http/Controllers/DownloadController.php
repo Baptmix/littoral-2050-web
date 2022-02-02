f23267daedab2698d734e7cc0e2e7e8eca0af448
Baptmix <baptiste.jourq1@gmail.com>
2022-02-02 11:01:18 +0100

    Ajout application

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -7,6 +7,6 @@ class DownloadController extends Controller
 {
 
     public function ios() {
-        header( "Location: https://itms-services://?action=download-manifest&url=https://littoral-app.fr/app/manifest.plist");
+        header( "Location: itms-services://?action=download-manifest&url=https://littoral-app.fr/app/manifest.plist");
     }
 }
